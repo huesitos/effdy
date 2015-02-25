45edfb5d56b99059808cc646fb919abb5f281df1
Denisse Lara <dmlaramartin@gmail.com>
2015-02-24 20:44:08 -0600

    Added validations and relations

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,4 +3,8 @@ class Card
   field :question, type: String
   field :answer, type: String
   field :box, type: Integer
+  belongs_to :topic
+
+  validates :question, presence: true, length: { minimum: 2 }
+  validates :answer, presence: true
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,8 @@
 class Topic
   include Mongoid::Document
   field :title, type: String
+  has_many :cards
+
+  validates :title, presence: true, length: { minimum: 2 }
+  validates_associated :cards
 end
