7bb3aadd93b15beb40a0814c356811c525c745d6
Denisse Lara <dmlaramartin@gmail.com>
2015-02-28 14:44:30 -0600

    Destroy all topics before seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Topic.all.destroy
+
 topic = Topic.create(title: 'Capital cities')
 topic.cards.create([{question: 'Algeria', answer: 'Algiers', box: 1, topic_id: topic._id}, {question: 'Argentina', answer: 'Buenos Aires', box: 1, topic_id: topic._id}, {question: 'Armenia', answer: 'Yerevan', box: 1, topic_id: topic._id}, {question: 'Bolivia', answer: 'La Paz', box: 1, topic_id: topic._id}, {question: 'Brazil', answer: 'Brasilia', box: 1, topic_id: topic._id}, {question: 'Chile', answer: 'Santiago', box: 1, topic_id: topic._id}, {question: 'China', answer: 'Beijing', box: 1, topic_id: topic._id}, {question: 'Colombia', answer: 'Bogota', box: 1, topic_id: topic._id}, {question: 'Dominican Republic', answer: 'Santo Domingo', box: 1, topic_id: topic._id}, {question: 'Egypt', answer: 'Cairo', box: 1, topic_id: topic._id}, {question: 'France', answer: 'Paris', box: 1, topic_id: topic._id}])
 
