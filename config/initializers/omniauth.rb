49762c0df45e5a70fa44e90ca14a5e765be66db6
Denisse Lara <dmlaramartin@gmail.com>
2015-04-08 21:15:17 -0500

    Add omniauth configuration file

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,10 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  # Keep keys out of your Git repository! This uses environment variables from your
+  # shell's configuration file (e.g., .zshrc if you're using zsh). You'll need lines
+  # like
+  # export WANTADS_TWITTER_KEY=a2dfOXZJgdfdsb888asdf
+  # export WANTADS_TWITTER_SECRET=asdfabaj8834AFJDKFasdfasjjksdfsdlkljsdfsd
+  # in your .zshrc file. In order to obatin your Twitter Key and Twitter Secret,
+  # you need to first log into https://dev.twitter.com/ and create a new application
+  provider :twitter, ENV['APLUS_TWITTER_KEY'], ENV['APLUS_TWITTER_SECRET']
+end
