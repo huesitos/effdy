61bf6220fee628c8fc54c1d0c63f1851351c3483
Denisse Lara <dmlaramartin@gmail.com>
2015-06-12 11:30:10 -0400

    Edit topic model and controllers to remove everything that relates to review configuration or boxes. In topic controller, set_reviewing method now sets reviewing without calling any class method. In topic model, remove all the method that update review_boxes and review_boxes dates. Also, add recall_percentage with a default of 80%. In addition, update the comments for docummentation.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,22 +1,22 @@
-# A topic is used to organize cards. It has a title, a review configuration,
-# and three review boxes. Both the title and review configuration must be present.
-# It may belong to a subject. It can be set for reviewing, which enables the
-# review boxes to be shown in today study view. If a topic is destroyed, its
-# cards and review boxes are also destroyed. It may belong to a subject. If
-# that subject is archived, then the topic is also archived.
+# A topic is used to organize cards. It has a title, and a recall_percentage
+# The title must be present. Also, the recall_percentage must be a number between
+# 0 and 1, including them. It may belong to a subject. It can be set for reviewing.
+# When a topic is set for reviewing, the user receives a notification when any card's
+# review date is today. If a topic is destroyed, its cards are also destroyed.
+# If the subject it belongs to is archived, then the topic is also archived.
 class Topic
   include Mongoid::Document
   field :title, type: String
   field :reviewing, type: Boolean, default: false
-  field :review_configuration, type: String, default: 'normal'
   field :archived, type: Boolean, default: false
+  field :recall_percentage, type: Integer, default: 0.80
+
   has_many :cards, dependent: :destroy
-  has_many :review_boxes, dependent: :destroy
   belongs_to :subject
   belongs_to :user
 
-  validates :title, length: { minimum: 2 }
-  validates :title, :review_configuration, presence: true
+  validates :title, presence: true
+  validates :recall_percentage, numericality: { greater_than: 0, less_than_or_equal_to: 1 }
   validates_associated :cards
 
   scope :not_archived, ->{where(archived: false)}
@@ -35,38 +35,6 @@ class Topic
     end
   end
 
-  # Creates three review boxes for the topic.
-  def self.set_review_boxes(topic)
-    topic.review_boxes.create(box:1, user_id: topic.user_id, review_date: "")
-    topic.review_boxes.create(box:2, user_id: topic.user_id, review_date: "")
-    topic.review_boxes.create(box:3, user_id: topic.user_id, review_date: "")
-  end
-
-  # Set topic and its review boxes as reviewing
-  def self.set_reviewing(topic)
-    topic.update(reviewing: true)
-    topic.review_boxes.each do |rb|
-      rb.update(reviewing: true)
-    end
-  end
-
-  # Unset topic and its review boxes as reviewing
-  def self.unset_reviewing(topic)
-    topic.update(reviewing: false)
-    topic.review_boxes.each do |rb|
-      rb.update(reviewing: false)
-    end
-  end
-
-  # Sets the review dates of the boxes based on the topic's review configuration.
-  def self.set_review_dates(topic)
-    config = ReviewConfiguration.find_by(name: topic.review_configuration)
-
-    topic.review_boxes.find_by(box:1).update(review_date: Date.today)
-    topic.review_boxes.find_by(box:2).update(review_date: (Date.today + config.box_frequencies[1]).to_s)
-    topic.review_boxes.find_by(box:3).update(review_date: (Date.today + config.box_frequencies[2]).to_s)
-  end
-
   # Shares the topic that belongs to another user, with the current user
   # It creates a new copy of the topic that belongs to the current user
   def self.share(topic, username, subject)
@@ -74,18 +42,14 @@ class Topic
 
     # makes a copy of the topic for the current user
     new_topic = user.topics.create(title: topic.title,
-      reviewing: false,
-      review_configuration: topic.review_configuration,
-      archived: false,
       subject: subject._id)
 
     Topic.set_review_boxes new_topic
 
     # copies all the cards in topic to the new topic
     topic.cards.each do |card|
-      new_topic.cards.create(question: card.question,
-        answer: card.answer,
-        box: card.box,
+      new_topic.cards.create(front: card.front,
+        back: card.back,
         user_id: user._id)
     end
   end
